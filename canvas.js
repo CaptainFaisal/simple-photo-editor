885394dcf554f20da51034f6a1600f566bdd9746
FaisalHawlader <faisalhawlader4@gmail.com>
2020-07-10 16:31:34 +0600

    'custom-image'

--- canvas.js
+++ canvas.js
@@ -7,6 +7,7 @@ window.onload = async () => {
   const ctx2 = canvas2.getContext("2d");
   const ctx3 = canvas3.getContext("2d");
   const ctx4 = canvas4.getContext("2d");
+  const inpFile = document.getElementById("inpFile");
   const loadImage = (url) => {
     return new Promise((r) => {
       let i = new Image();
@@ -14,7 +15,7 @@ window.onload = async () => {
       i.src = url;
     });
   };
-  const img = await loadImage("facebook.PNG");
+  let img = "";
   const img1 = await loadImage("T-Shirt.jpg");
   const img2 = await loadImage("curtain.jpg");
   const img3 = await loadImage("water.jpg");
@@ -22,33 +23,49 @@ window.onload = async () => {
   const getWidth = (img, height) => {
     return height * (img.width / img.height);
   };
-  //T-Shirt
-  canvas1.height = 500;
-  canvas1.width = getWidth(img1, 500);
-  ctx1.globalCompositeOperation = "multiply";
-  ctx1.drawImage(img1, 0, 0, getWidth(img1, 500), 500);
-  ctx1.translate(90, 120);
-  ctx1.drawImage(img, 0, 0, getWidth(img, 320), 320);
-  //Curtain
-  canvas2.height = 500;
-  canvas2.width = getWidth(img2, 500);
-  ctx2.globalCompositeOperation = "multiply";
-  ctx2.drawImage(img2, 0, 0, getWidth(img2, 400), 400);
-  ctx2.drawImage(img, canvas2.width / 2 + 30, 30, getWidth(img, 320), 320);
-  //Water
-  canvas3.height = 500;
-  canvas3.width = getWidth(img3, 500);
-  ctx3.globalCompositeOperation = "multiply";
-  ctx3.drawImage(img3, 0, 0, getWidth(img3, 400), 400);
-  ctx3.translate(100, 100);
-  ctx3.rotate((-30 * Math.PI) / 180);
-  ctx3.drawImage(img, 0, 0, getWidth(img, 320), 320);
-  //hammer
-  canvas4.height = 500;
-  canvas4.width = getWidth(img4, 500);
-  ctx4.globalCompositeOperation = "multiply";
-  ctx4.drawImage(img4, 0, 0, getWidth(img4, 400), 400);
-  ctx4.transform(1, 0.13, 0.2, 1, 335, 180);
-  ctx4.rotate((-15 * Math.PI) / 180);
-  ctx4.drawImage(img, 0, 0, getWidth(img, 250), 180);
+  inpFile.addEventListener("change", function () {
+    const file = this.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.addEventListener("load", async function () {
+        img = await loadImage(this.result);
+        //T-Shirt
+        canvas1.height = 500;
+        canvas1.width = getWidth(img1, 500);
+        ctx1.globalCompositeOperation = "multiply";
+        ctx1.drawImage(img1, 0, 0, getWidth(img1, 500), 500);
+        ctx1.translate(90, 120);
+        ctx1.drawImage(img, 0, 0, getWidth(img, 320), 320);
+        //Curtain
+        canvas2.height = 500;
+        canvas2.width = getWidth(img2, 500);
+        ctx2.globalCompositeOperation = "multiply";
+        ctx2.drawImage(img2, 0, 0, getWidth(img2, 400), 400);
+        ctx2.drawImage(
+          img,
+          canvas2.width / 2 + 30,
+          30,
+          getWidth(img, 320),
+          320
+        );
+        //Water
+        canvas3.height = 500;
+        canvas3.width = getWidth(img3, 500);
+        ctx3.globalCompositeOperation = "multiply";
+        ctx3.drawImage(img3, 0, 0, getWidth(img3, 400), 400);
+        ctx3.translate(100, 100);
+        ctx3.rotate((-30 * Math.PI) / 180);
+        ctx3.drawImage(img, 0, 0, getWidth(img, 320), 320);
+        //hammer
+        canvas4.height = 500;
+        canvas4.width = getWidth(img4, 500);
+        ctx4.globalCompositeOperation = "multiply";
+        ctx4.drawImage(img4, 0, 0, getWidth(img4, 400), 400);
+        ctx4.transform(1, 0.13, 0.1, 1, 335, 180);
+        ctx4.rotate((-15 * Math.PI) / 180);
+        ctx4.drawImage(img, 0, 0, getWidth(img, 250), 180);
+      });
+      reader.readAsDataURL(file);
+    }
+  });
 };
